2290a9f345acd11c0590d8266cd6b3a1cb22238b
porolakka <porolakka@gmail.com>
2020-09-10 19:53:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ const config = {
 		  </li>`,
     links: [{ text: '', link: '' }],
     search: {
-      enabled: false,
-      indexName: '',
+      enabled: true,
+      indexName: 'test_KHMA',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
